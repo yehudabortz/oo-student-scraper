ae81ba0732e8add78272de3ab2a5e35466d62bd2
Yehuda Bortz <yehudabortz@icloud.com>
2020-11-27 14:33:40 -0600

    -a

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -5,7 +5,7 @@ require 'colorize'
 
 class CommandLineInterface
   BASE_PATH = "https://learn-co-curriculum.github.io/student-scraper-test-page/"
-
+  
   def run
     make_students
     add_attributes_to_students
@@ -38,4 +38,5 @@ class CommandLineInterface
     end
   end
 
-end
+end 
+
